b285e0f263fe3ddf2ab1c07bf6dcaef42c59a3ff
cwatts6 <16717943+cwatts6@users.noreply.github.com>
2026-03-17 14:42:39 +0000

    Nightly SQL schema export 2026-03-17 14:42

---
+++ sql_schema/dbo.MGE_FinalResults.Table.sql
@@ -0,0 +1,54 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[MGE_FinalResults](
+	[FinalResultId] [bigint] IDENTITY(1,1) NOT NULL,
+	[ImportId] [bigint] NOT NULL,
+	[EventId] [bigint] NOT NULL,
+	[EventMode] [varchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
+	[Rank] [int] NOT NULL,
+	[PlayerId] [bigint] NOT NULL,
+	[PlayerName] [nvarchar](200) COLLATE Latin1_General_CI_AS NOT NULL,
+	[Score] [bigint] NOT NULL,
+	[ReconciliationStatus] [varchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
+	[CreatedUtc] [datetime2](7) NOT NULL,
+	[UpdatedUtc] [datetime2](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[FinalResultId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]') AND name = N'IX_MGE_FinalResults_EventRank')
+CREATE NONCLUSTERED INDEX [IX_MGE_FinalResults_EventRank] ON [dbo].[MGE_FinalResults]
+(
+	[EventId] ASC,
+	[Rank] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]') AND name = N'IX_MGE_FinalResults_Player')
+CREATE NONCLUSTERED INDEX [IX_MGE_FinalResults_Player] ON [dbo].[MGE_FinalResults]
+(
+	[PlayerId] ASC,
+	[EventId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DF__MGE_Final__Creat__4A92283B]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[MGE_FinalResults] ADD  DEFAULT (sysutcdatetime()) FOR [CreatedUtc]
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DF__MGE_Final__Updat__4B864C74]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[MGE_FinalResults] ADD  DEFAULT (sysutcdatetime()) FOR [UpdatedUtc]
+END
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_MGE_FinalResults_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]'))
+ALTER TABLE [dbo].[MGE_FinalResults]  WITH CHECK ADD  CONSTRAINT [FK_MGE_FinalResults_Event] FOREIGN KEY([EventId])
+REFERENCES [dbo].[MGE_Events] ([EventId])
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_MGE_FinalResults_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]'))
+ALTER TABLE [dbo].[MGE_FinalResults] CHECK CONSTRAINT [FK_MGE_FinalResults_Event]
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_MGE_FinalResults_Import]') AND parent_object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]'))
+ALTER TABLE [dbo].[MGE_FinalResults]  WITH CHECK ADD  CONSTRAINT [FK_MGE_FinalResults_Import] FOREIGN KEY([ImportId])
+REFERENCES [dbo].[MGE_ResultImports] ([ImportId])
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_MGE_FinalResults_Import]') AND parent_object_id = OBJECT_ID(N'[dbo].[MGE_FinalResults]'))
+ALTER TABLE [dbo].[MGE_FinalResults] CHECK CONSTRAINT [FK_MGE_FinalResults_Import]
---
+++ sql_schema/dbo.MGE_ResultImports.Table.sql
@@ -0,0 +1,57 @@
+﻿SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MGE_ResultImports]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[MGE_ResultImports](
+	[ImportId] [bigint] IDENTITY(1,1) NOT NULL,
+	[EventId] [bigint] NOT NULL,
+	[EventMode] [varchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
+	[SourceType] [varchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
+	[Filename] [nvarchar](255) COLLATE Latin1_General_CI_AS NOT NULL,
+	[FileHashSha256] [char](64) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ImportStatus] [varchar](20) COLLATE Latin1_General_CI_AS NOT NULL,
+	[RowCount] [int] NULL,
+	[ErrorText] [nvarchar](1000) COLLATE Latin1_General_CI_AS NULL,
+	[ActorDiscordId] [bigint] NULL,
+	[DetailsJson] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[CreatedUtc] [datetime2](7) NOT NULL,
+	[UpdatedUtc] [datetime2](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ImportId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
+SET ANSI_PADDING ON
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[MGE_ResultImports]') AND name = N'IX_MGE_ResultImports_EventHash')
+CREATE NONCLUSTERED INDEX [IX_MGE_ResultImports_EventHash] ON [dbo].[MGE_ResultImports]
+(
+	[EventId] ASC,
+	[FileHashSha256] ASC,
+	[ImportStatus] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+SET ANSI_PADDING ON
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[MGE_ResultImports]') AND name = N'IX_MGE_ResultImports_EventStatus')
+CREATE NONCLUSTERED INDEX [IX_MGE_ResultImports_EventStatus] ON [dbo].[MGE_ResultImports]
+(
+	[EventId] ASC,
+	[ImportStatus] ASC,
+	[CreatedUtc] DESC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DF__MGE_Resul__Creat__45CD731E]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[MGE_ResultImports] ADD  DEFAULT (sysutcdatetime()) FOR [CreatedUtc]
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DF__MGE_Resul__Updat__46C19757]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[MGE_ResultImports] ADD  DEFAULT (sysutcdatetime()) FOR [UpdatedUtc]
+END
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_MGE_ResultImports_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[MGE_ResultImports]'))
+ALTER TABLE [dbo].[MGE_ResultImports]  WITH CHECK ADD  CONSTRAINT [FK_MGE_ResultImports_Event] FOREIGN KEY([EventId])
+REFERENCES [dbo].[MGE_Events] ([EventId])
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_MGE_ResultImports_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[MGE_ResultImports]'))
+ALTER TABLE [dbo].[MGE_ResultImports] CHECK CONSTRAINT [FK_MGE_ResultImports_Event]
