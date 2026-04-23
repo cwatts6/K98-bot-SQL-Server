cec4a90dc9b89de2c45e39b705b35bc0a883f8ca
cwatts6 <16717943+cwatts6@users.noreply.github.com>
2026-04-23 01:31:37 +0100

    Nightly SQL schema export 2026-04-23 01:31

--- sql_schema/dbo.v_EXCEL_FOR_KVK_All.View.sql
+++ sql_schema/dbo.v_EXCEL_FOR_KVK_All.View.sql
@@ -889,5 +889,79 @@ SELECT
 
     [KVK_NO]
 FROM dbo.[EXCEL_FOR_KVK_14]
+UNION ALL
+
+SELECT
+    [Rank],
+    [KVK_RANK],
+    [Gov_ID],
+    [Governor_Name],
+    [Starting Power],
+    [Power_Delta],
+
+   [Civilization] AS [Civilization],
+    [KvKPlayed],
+    [MostKvKKill],
+    [MostKvKDead],
+    [MostKvKHeal],
+    [Acclaim],
+    [HighestAcclaim],
+    [AOOJoined],
+    [AOOWon],
+    [AOOAvgKill],
+    [AOOAvgDead],
+    [AOOAvgHeal],
+
+    [Starting_T4&T5_KILLS],
+    [T4_KILLS],
+    [T5_KILLS],
+    [T4&T5_Kills],
+    [KILLS_OUTSIDE_KVK],
+    [Kill Target],
+    [% of Kill Target]        AS [% of Kill Target],
+
+    [Starting_Deads]     AS [Starting_Deads],
+    [Deads_Delta]     AS [Deads_Delta],
+    [DEADS_OUTSIDE_KVK],
+    [T4_Deads],
+    [T5_Deads],
+    [Dead_Target]     AS [Dead_Target],
+    [% of Dead Target]  AS [% of Dead Target],
+
+    [Zeroed],
+    [DKP_SCORE]            AS [DKP_SCORE],
+    [DKP Target],
+    [% of DKP Target]         AS [% of DKP Target],
+
+    [HelpsDelta]          AS [HelpsDelta],
+    [RSS_Assist_Delta]      AS [RSS_Assist_Delta],
+    [RSS_Gathered_Delta]    AS [RSS_Gathered_Delta],
+
+    [Pass 4 Kills],
+    [Pass 6 Kills],
+    [Pass 7 Kills],
+    [Pass 8 Kills],
+    [Pass 4 Deads],
+    [Pass 6 Deads],
+    [Pass 7 Deads],
+    [Pass 8 Deads],
+
+    [Starting_HealedTroops]    AS [Starting_HealedTroops],
+    [HealedTroopsDelta]    AS [HealedTroopsDelta],
+    [Starting_KillPoints]      AS [Starting_KillPoints],
+    [KillPointsDelta] AS [KillPointsDelta],
+
+    [RangedPoints]         AS [RangedPoints],
+    [RangedPointsDelta]    AS [RangedPointsDelta],
+
+    [AutarchTimes]   AS [AutarchTimes],
+
+    [Max_PreKvk_Points]      AS [Max_PreKvk_Points],
+    [Max_HonorPoints]       AS [Max_HonorPoints],
+    [PreKvk_Rank]     AS [PreKvk_Rank],
+    [Honor_Rank]      AS [Honor_Rank],
+
+    [KVK_NO]
+FROM dbo.[EXCEL_FOR_KVK_15]
 
 '
