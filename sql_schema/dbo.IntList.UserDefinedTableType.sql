acde9677de98a661c0dc67ea3c210b2e5e0695d1
cwatts6 <16717943+cwatts6@users.noreply.github.com>
2026-05-12 23:13:21 +0100

    Nightly SQL schema export 2026-05-12 23:13

---
+++ sql_schema/dbo.IntList.UserDefinedTableType.sql
@@ -0,0 +1,8 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.types st JOIN sys.schemas ss ON st.schema_id = ss.schema_id WHERE st.name = N'IntList' AND ss.name = N'dbo')
+CREATE TYPE [dbo].[IntList] AS TABLE(
+	[ID] [int] NOT NULL,
+	PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (IGNORE_DUP_KEY = OFF)
+)
---
+++ sql_schema/dbo.NVarCharList.UserDefinedTableType.sql
@@ -0,0 +1,4 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.types st JOIN sys.schemas ss ON st.schema_id = ss.schema_id WHERE st.name = N'NVarCharList' AND ss.name = N'dbo')
+CREATE TYPE [dbo].[NVarCharList] AS TABLE(
+	[Value] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL
+)
