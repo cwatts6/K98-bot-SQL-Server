36f316b60ab0717edcee5db59199cbb415479603
cwatts6 <16717943+cwatts6@users.noreply.github.com>
2026-06-02 16:33:38 +0100

    modified:   sql_schema/dbo.DeploymentRunHistory.Table.sql modified:   sql_schema/dbo.SchemaMigrationHistory.Table.sql

--- sql_schema/dbo.DeploymentRunHistory.Table.sql
+++ sql_schema/dbo.DeploymentRunHistory.Table.sql
@@ -1,32 +1,33 @@
-SET ANSI_NULLS ON
+﻿SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DeploymentRunHistory]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [dbo].[DeploymentRunHistory](
-    [DeploymentId] [uniqueidentifier] NOT NULL,
-    [StartedAtUtc] [datetime2](0) NOT NULL,
-    [FinishedAtUtc] [datetime2](0) NULL,
-    [StartedBy] [nvarchar](255) NULL,
-    [MachineName] [nvarchar](255) NULL,
-    [DatabaseName] [nvarchar](255) NULL,
-    [GitCommit] [nvarchar](40) NULL,
-    [BranchName] [nvarchar](255) NULL,
-    [MigrationCount] [int] NOT NULL,
-    [Status] [nvarchar](50) NOT NULL,
-    [ErrorMessage] [nvarchar](max) NULL,
-    [DurationSeconds] [int] NULL,
- CONSTRAINT [PK_DeploymentRunHistory] PRIMARY KEY CLUSTERED
+	[DeploymentId] [uniqueidentifier] NOT NULL,
+	[StartedAtUtc] [datetime2](0) NOT NULL,
+	[FinishedAtUtc] [datetime2](0) NULL,
+	[StartedBy] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[MachineName] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[DatabaseName] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[GitCommit] [nvarchar](40) COLLATE Latin1_General_CI_AS NULL,
+	[BranchName] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[MigrationCount] [int] NOT NULL,
+	[Status] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ErrorMessage] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[DurationSeconds] [int] NULL,
+ CONSTRAINT [PK_DeploymentRunHistory] PRIMARY KEY CLUSTERED 
 (
-    [DeploymentId] ASC
+	[DeploymentId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 END
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[DeploymentRunHistory]') AND name = N'IX_DeploymentRunHistory_StartedAtUtc')
 CREATE NONCLUSTERED INDEX [IX_DeploymentRunHistory_StartedAtUtc] ON [dbo].[DeploymentRunHistory]
 (
-    [StartedAtUtc] DESC
+	[StartedAtUtc] DESC
 )
 INCLUDE([Status],[BranchName],[GitCommit],[MigrationCount]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-
 IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_DeploymentRunHistory_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[DeploymentRunHistory]'))
-ALTER TABLE [dbo].[DeploymentRunHistory] ADD CONSTRAINT [CK_DeploymentRunHistory_Status] CHECK (([Status]=N'ValidationOnly' OR [Status]=N'Failed' OR [Status]=N'Succeeded' OR [Status]=N'Started'))
+ALTER TABLE [dbo].[DeploymentRunHistory]  WITH CHECK ADD  CONSTRAINT [CK_DeploymentRunHistory_Status] CHECK  (([Status]=N'ValidationOnly' OR [Status]=N'Failed' OR [Status]=N'Succeeded' OR [Status]=N'Started'))
+IF  EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_DeploymentRunHistory_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[DeploymentRunHistory]'))
+ALTER TABLE [dbo].[DeploymentRunHistory] CHECK CONSTRAINT [CK_DeploymentRunHistory_Status]
--- sql_schema/dbo.SchemaMigrationHistory.Table.sql
+++ sql_schema/dbo.SchemaMigrationHistory.Table.sql
@@ -1,32 +1,33 @@
-SET ANSI_NULLS ON
+﻿SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SchemaMigrationHistory]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [dbo].[SchemaMigrationHistory](
-    [MigrationId] [nvarchar](255) NOT NULL,
-    [MigrationFile] [nvarchar](512) NOT NULL,
-    [ChecksumSha256] [nvarchar](64) NOT NULL,
-    [AppliedAtUtc] [datetime2](0) NOT NULL,
-    [AppliedBy] [nvarchar](255) NULL,
-    [MachineName] [nvarchar](255) NULL,
-    [GitCommit] [nvarchar](40) NULL,
-    [BranchName] [nvarchar](255) NULL,
-    [DeploymentId] [uniqueidentifier] NULL,
-    [Status] [nvarchar](50) NOT NULL,
-    [ErrorMessage] [nvarchar](max) NULL,
-    [DurationMs] [int] NULL,
- CONSTRAINT [PK_SchemaMigrationHistory] PRIMARY KEY CLUSTERED
+	[MigrationId] [nvarchar](255) COLLATE Latin1_General_CI_AS NOT NULL,
+	[MigrationFile] [nvarchar](512) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ChecksumSha256] [nvarchar](64) COLLATE Latin1_General_CI_AS NOT NULL,
+	[AppliedAtUtc] [datetime2](0) NOT NULL,
+	[AppliedBy] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[MachineName] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[GitCommit] [nvarchar](40) COLLATE Latin1_General_CI_AS NULL,
+	[BranchName] [nvarchar](255) COLLATE Latin1_General_CI_AS NULL,
+	[DeploymentId] [uniqueidentifier] NULL,
+	[Status] [nvarchar](50) COLLATE Latin1_General_CI_AS NOT NULL,
+	[ErrorMessage] [nvarchar](max) COLLATE Latin1_General_CI_AS NULL,
+	[DurationMs] [int] NULL,
+ CONSTRAINT [PK_SchemaMigrationHistory] PRIMARY KEY CLUSTERED 
 (
-    [MigrationId] ASC
+	[MigrationId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 END
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[SchemaMigrationHistory]') AND name = N'IX_SchemaMigrationHistory_DeploymentId')
 CREATE NONCLUSTERED INDEX [IX_SchemaMigrationHistory_DeploymentId] ON [dbo].[SchemaMigrationHistory]
 (
-    [DeploymentId] ASC
+	[DeploymentId] ASC
 )
 INCLUDE([MigrationId],[Status],[AppliedAtUtc]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-
 IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_SchemaMigrationHistory_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[SchemaMigrationHistory]'))
-ALTER TABLE [dbo].[SchemaMigrationHistory] ADD CONSTRAINT [CK_SchemaMigrationHistory_Status] CHECK (([Status]=N'Skipped' OR [Status]=N'Failed' OR [Status]=N'Applied' OR [Status]=N'Pending'))
+ALTER TABLE [dbo].[SchemaMigrationHistory]  WITH CHECK ADD  CONSTRAINT [CK_SchemaMigrationHistory_Status] CHECK  (([Status]=N'Skipped' OR [Status]=N'Failed' OR [Status]=N'Applied' OR [Status]=N'Pending'))
+IF  EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_SchemaMigrationHistory_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[SchemaMigrationHistory]'))
+ALTER TABLE [dbo].[SchemaMigrationHistory] CHECK CONSTRAINT [CK_SchemaMigrationHistory_Status]
