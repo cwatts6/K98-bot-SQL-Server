30e66f58ea41ced5b931e200caa74e25b10728f8
cwatts6 <16717943+cwatts6@users.noreply.github.com>
2026-07-02 01:31:41 +0100

    chore: export production SQL schema 20260702-013004

--- sql_schema/dbo.ImportAuditBatch.Table.sql
+++ sql_schema/dbo.ImportAuditBatch.Table.sql
@@ -1,4 +1,4 @@
-SET ANSI_NULLS ON
+﻿SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]') AND type in (N'U'))
 BEGIN
@@ -38,14 +38,8 @@ CREATE TABLE [dbo].[ImportAuditBatch](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 END
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]') AND name = N'IX_ImportAuditBatch_KindStatusStarted')
-CREATE NONCLUSTERED INDEX [IX_ImportAuditBatch_KindStatusStarted] ON [dbo].[ImportAuditBatch]
-(
-	[ImportKind] ASC,
-	[Status] ASC,
-	[StartedAtUtc] DESC
-)
-INCLUDE([SourceFilename],[ExternalBatchTable],[ExternalBatchId],[RowsWritten]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+SET ANSI_PADDING ON
+
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]') AND name = N'IX_ImportAuditBatch_ExternalBatch')
 CREATE NONCLUSTERED INDEX [IX_ImportAuditBatch_ExternalBatch] ON [dbo].[ImportAuditBatch]
 (
@@ -55,6 +49,16 @@ CREATE NONCLUSTERED INDEX [IX_ImportAuditBatch_ExternalBatch] ON [dbo].[ImportAu
 )
 WHERE ([ExternalBatchTable] IS NOT NULL AND [ExternalBatchId] IS NOT NULL)
 WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+SET ANSI_PADDING ON
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]') AND name = N'IX_ImportAuditBatch_KindStatusStarted')
+CREATE NONCLUSTERED INDEX [IX_ImportAuditBatch_KindStatusStarted] ON [dbo].[ImportAuditBatch]
+(
+	[ImportKind] ASC,
+	[Status] ASC,
+	[StartedAtUtc] DESC
+)
+INCLUDE([SourceFilename],[ExternalBatchTable],[ExternalBatchId],[RowsWritten]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DF_ImportAuditBatch_CorrelationId]') AND type = 'D')
 BEGIN
 ALTER TABLE [dbo].[ImportAuditBatch] ADD  CONSTRAINT [DF_ImportAuditBatch_CorrelationId]  DEFAULT (newid()) FOR [CorrelationId]
@@ -75,11 +79,11 @@ BEGIN
 ALTER TABLE [dbo].[ImportAuditBatch] ADD  CONSTRAINT [DF_ImportAuditBatch_UpdatedAtUtc]  DEFAULT (sysutcdatetime()) FOR [UpdatedAtUtc]
 END
 
-IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
-ALTER TABLE [dbo].[ImportAuditBatch] WITH CHECK ADD CONSTRAINT [CK_ImportAuditBatch_Status] CHECK (([Status]=N'rolled_back' OR [Status]=N'cancelled' OR [Status]=N'duplicate' OR [Status]=N'skipped' OR [Status]=N'failed' OR [Status]=N'completed' OR [Status]=N'downstream_rebuild_started' OR [Status]=N'procedure_started' OR [Status]=N'converted' OR [Status]=N'staged' OR [Status]=N'started' OR [Status]=N'queued'))
-IF EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
-ALTER TABLE [dbo].[ImportAuditBatch] CHECK CONSTRAINT [CK_ImportAuditBatch_Status]
 IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_DetailsJson]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
-ALTER TABLE [dbo].[ImportAuditBatch] WITH CHECK ADD CONSTRAINT [CK_ImportAuditBatch_DetailsJson] CHECK (([DetailsJson] IS NULL OR isjson([DetailsJson])=(1)))
-IF EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_DetailsJson]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
+ALTER TABLE [dbo].[ImportAuditBatch]  WITH CHECK ADD  CONSTRAINT [CK_ImportAuditBatch_DetailsJson] CHECK  (([DetailsJson] IS NULL OR isjson([DetailsJson])=(1)))
+IF  EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_DetailsJson]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
 ALTER TABLE [dbo].[ImportAuditBatch] CHECK CONSTRAINT [CK_ImportAuditBatch_DetailsJson]
+IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
+ALTER TABLE [dbo].[ImportAuditBatch]  WITH CHECK ADD  CONSTRAINT [CK_ImportAuditBatch_Status] CHECK  (([Status]=N'rolled_back' OR [Status]=N'cancelled' OR [Status]=N'duplicate' OR [Status]=N'skipped' OR [Status]=N'failed' OR [Status]=N'completed' OR [Status]=N'downstream_rebuild_started' OR [Status]=N'procedure_started' OR [Status]=N'converted' OR [Status]=N'staged' OR [Status]=N'started' OR [Status]=N'queued'))
+IF  EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[CK_ImportAuditBatch_Status]') AND parent_object_id = OBJECT_ID(N'[dbo].[ImportAuditBatch]'))
+ALTER TABLE [dbo].[ImportAuditBatch] CHECK CONSTRAINT [CK_ImportAuditBatch_Status]
