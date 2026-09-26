77d7dd7d7b9134868ab9fef344418e6bbd96d38d
cwatts6 <16717943+cwatts6@users.noreply.github.com>
2026-09-26 01:31:58 +0100

    chore: export production SQL schema 20260926-013005

--- sql_schema/dbo.vw_Governor_KVK_Summary_GlobalLatest.View.sql
+++ sql_schema/dbo.vw_Governor_KVK_Summary_GlobalLatest.View.sql
@@ -3,50 +3,45 @@ SET QUOTED_IDENTIFIER ON
 IF NOT EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[vw_Governor_KVK_Summary_GlobalLatest]'))
 EXECUTE dbo.sp_executesql N'
 CREATE VIEW [dbo].[vw_Governor_KVK_Summary_GlobalLatest]  AS 
-WITH latest_scan AS (
-    -- only rows from the single most-recent scan (global max SCANORDER)
+WITH latest_scan AS
+(
     SELECT k.*
-    FROM dbo.KingdomScanData4 k
+    FROM dbo.KingdomScanData4 AS k
     WHERE k.SCANORDER = (SELECT MAX(SCANORDER) FROM dbo.KingdomScanData4)
 ),
-excel_ranked AS (
-    -- rank EXCEL_FOR_DASHBOARD rows per governor by KVK_RANK desc:
-    -- rn = 1 is Latest; rn = 2 is Last (previous)
-    SELECT e.*,
-           ROW_NUMBER() OVER (PARTITION BY e.Gov_ID ORDER BY e.KVK_NO DESC) AS rn
-    FROM dbo.EXCEL_FOR_DASHBOARD e
+excel_ranked AS
+(
+    SELECT
+        e.*,
+        ROW_NUMBER() OVER (PARTITION BY e.Gov_ID ORDER BY e.KVK_NO DESC) AS rn
+    FROM dbo.EXCEL_FOR_DASHBOARD AS e
 ),
-kvk_latest AS (
-    SELECT Gov_ID,
-           KVK_RANK,
-           [T4&T5_Kills],
-           [% of Kill Target]
+kvk_latest AS
+(
+    SELECT Gov_ID, KVK_RANK, [T4&T5_Kills], [% of Kill Target]
     FROM excel_ranked
     WHERE rn = 1
 ),
-kvk_prev AS (
-    SELECT Gov_ID,
-           KVK_RANK,
-           [T4&T5_Kills],
-           [% of Kill Target]
+kvk_prev AS
+(
+    SELECT Gov_ID, KVK_RANK, [T4&T5_Kills], [% of Kill Target]
     FROM excel_ranked
     WHERE rn = 2
 )
 SELECT
-    CAST(ls.GovernorID AS bigint)            AS [GovernorId],
-    ls.governorname AS [GovernorName],
+    CAST(ls.GovernorID AS bigint) AS GovernorId,
+    ls.GovernorName,
     ls.Power,
-    kvk_latest.KVK_RANK                     AS Latest_KVK_RANK,
-    kvk_prev.KVK_RANK                       AS Last_KVK_RANK,
-    kvk_latest.[T4&T5_Kills]                AS Latest_T4T5_Kills,
-    kvk_prev.[T4&T5_Kills]                  AS Last_T4T5_Kills,
-    kvk_latest.[% of Kill Target]           AS Latest_Percent_of_Kill_Target,
-    kvk_prev.[% of Kill Target]             AS Last_Percent_of_Kill_Target
-FROM latest_scan ls
+    kvk_latest.KVK_RANK AS Latest_KVK_RANK,
+    kvk_prev.KVK_RANK AS Last_KVK_RANK,
+    kvk_latest.[T4&T5_Kills] AS Latest_T4T5_Kills,
+    kvk_prev.[T4&T5_Kills] AS Last_T4T5_Kills,
+    kvk_latest.[% of Kill Target] AS Latest_Percent_of_Kill_Target,
+    kvk_prev.[% of Kill Target] AS Last_Percent_of_Kill_Target
+FROM latest_scan AS ls
 LEFT JOIN kvk_latest
-    ON kvk_latest.Gov_ID = ls.GovernorID
+  ON kvk_latest.Gov_ID = ls.GovernorID
 LEFT JOIN kvk_prev
-    ON kvk_prev.Gov_ID   = ls.GovernorID;
-
+  ON kvk_prev.Gov_ID = ls.GovernorID;
 
 '
